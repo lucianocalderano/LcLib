2336508a4fd6c1d0943c2e5f1053150aeb2f6d81
Luciano <luciano.calderano@mobilesoft.it>
2018-07-27 16:43:20 +0200

    Initial Commit

---
+++ LcLib/LcLib.h
@@ -0,0 +1,19 @@
+//
+//  LcLib.h
+//  LcLib
+//
+//  Created by Developer on 27/07/18.
+//  Copyright © 2018 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LcLib.
+FOUNDATION_EXPORT double LcLibVersionNumber;
+
+//! Project version string for LcLib.
+FOUNDATION_EXPORT const unsigned char LcLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LcLib/PublicHeader.h>
+
+
